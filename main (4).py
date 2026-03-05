c56da15b6492d34b892acc6be69baee6b99cbb6c
ManuelaAfonso <reilacbds@gmail.com>
2026-03-04 21:56:29 -0300

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+raio = float(input())
+pi = 3.14159
+area = pi * raio * raio
+print(f"A={area:.4f}")
