8c372200a89f5407a6d161693edeed92dc246f41
ManuelaAfonso <reilacbds@gmail.com>
2026-03-07 22:33:21 -0300

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+A = float(input())
+B = float(input())
+media = (A * 3.5 + B * 7.5) / 11.0
+print(f"MEDIA = {media:.5f}")
