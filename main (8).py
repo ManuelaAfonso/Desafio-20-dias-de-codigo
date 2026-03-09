24365c25afa870739cb3c831556077f301059dd2
ManuelaAfonso <reilacbds@gmail.com>
2026-03-08 22:48:24 -0300

    Add files via upload

---
+++ main (8).py
@@ -0,0 +1,16 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+
+A = float(input().strip())
+B = float(input().strip())
+C = float(input().strip())
+media = (A*2 + B*3 + C*5) / 10
+print(f"MEDIA = {media:.1f}")
+
