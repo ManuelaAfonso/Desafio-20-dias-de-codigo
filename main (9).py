bce83972c88f598112c11822bae71bf7274efce1
ManuelaAfonso <reilacbds@gmail.com>
2026-03-09 21:44:20 -0300

    Add files via upload

---
+++ main (9).py
@@ -0,0 +1,18 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+A = int(input().strip())
+B = int(input().strip())
+C = int(input().strip())
+D = int(input().strip())
+
+diferenca = (A * B) - (C * D)
+
+print(f"DIFERENCA = {diferenca}")
+
+
