d8ed20449a2ca2203fb450fc36ea9175b0746ce1
ManuelaAfonso <reilacbds@gmail.com>
2026-03-10 17:38:52 -0300

    Add files via upload

---
+++ main (10).py
@@ -0,0 +1,21 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+num = int(input().strip())
+horas = int(input().strip())
+valor = float(input().strip())
+
+salario = horas * valor
+
+print(f"NUMBER = {num}")
+print(f"SALARY = U$ {salario:.2f}")
+
+
+
+
