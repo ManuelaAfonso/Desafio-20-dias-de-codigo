a95d2c10216d199f9891fd4e3d296513d5d3978b
ManuelaAfonso <reilacbds@gmail.com>
2026-03-11 20:50:36 -0300

    Add files via upload

---
+++ main (11).py
@@ -0,0 +1,22 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+nome = input().strip()          
+salario_fixo = float(input().strip())
+vendas = float(input().strip())
+
+total = salario_fixo + 0.15 * vendas
+
+print(f"TOTAL = R$ {total:.2f}")
+
+
+
+
+
+
+
