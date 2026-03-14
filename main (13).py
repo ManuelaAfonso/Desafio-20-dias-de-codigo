f6c5426a75230ee00cbd62dc4821d4ce397251b0
ManuelaAfonso <reilacbds@gmail.com>
2026-03-13 22:53:05 -0300

    Add files via upload

---
+++ main (13).py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+PI = 3.14159
+R = float(input().strip())
+volume = (4.0/3.0) * PI * (R ** 3)
+print(f"VOLUME = {volume:.3f}")
+
