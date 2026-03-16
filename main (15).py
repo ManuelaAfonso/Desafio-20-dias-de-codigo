b9da19cff962b800da443a45c6ef82e7d22cd8bf
ManuelaAfonso <reilacbds@gmail.com>
2026-03-15 21:34:54 -0300

    Add files via upload

---
+++ main (15).py
@@ -0,0 +1,10 @@
+
+a, b, c = map(int, input().split())
+
+def maior(x, y):
+    return (x + y + abs(x - y)) // 2  # inteiro
+
+maior_ab = maior(a, b)
+maior_abc = maior(maior_ab, c)
+
+print(f"{maior_abc} eh o maior")
